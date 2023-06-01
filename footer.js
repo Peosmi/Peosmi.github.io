50a6390bb99a684255cb113d9cc291503862efb5
Pen <peonysmithpm@gmail.com>
2023-06-01 13:17:45 +1200

    Initial Commit

---
+++ footer.js
@@ -0,0 +1,4 @@
+document.write('<footer><ul class="list-inline" style="padding-left: 0"><li class="list-inline-item"><a href="index.html"><p ' +
+    'class="copyright">Home</a></li> ' +
+    '<li class="list-inline-item"><a ' +
+    'href="terms&Conditions.html">Terms & Conditions</a></li></ul>The Designer Vault © 2023</p></footer>')
